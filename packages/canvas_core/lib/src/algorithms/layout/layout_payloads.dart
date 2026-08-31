8d9b6e19f3e0898044aef5050376c945e0c6163b
bhav-yeah <107114820+bhav-yeah@users.noreply.github.com>
2026-08-31 16:28:36 +0200

    refactor(core): simplify layout payloads

--- packages/canvas_core/lib/src/algorithms/layout/layout_payloads.dart
+++ packages/canvas_core/lib/src/algorithms/layout/layout_payloads.dart
@@ -2,21 +2,4 @@
 
 import 'package:canvas_core/src/foundation/geometry/geometry.dart';
 
-final class ImagePlacement {
-  final Rect2D src;
-  final Rect2D dst;
-
-  const ImagePlacement({required this.src, required this.dst});
-}
-
-final class IconTextPayload {
-  final String glyph;
-  final String family;
-  final int weight;
-
-  const IconTextPayload({
-    required this.glyph,
-    required this.family,
-    required this.weight,
-  });
-}
+typedef ImagePlacement = ({Rect2D src, Rect2D dst});
